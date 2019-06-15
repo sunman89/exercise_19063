c28961426f09e7ff410a38ffefa506b10c3c85e9
sunman89 <sonnystokes@hotmail.com>
2019-06-15 23:57:13 +0100

    Finished the test

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -6,3 +6,17 @@ it('when the system date and the date to format are the same day formats as "TOD
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
   expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
 });
+
+it('when the system date and the date to format are not the same day formats as "16/06/2019"', () => {
+  const June = 5; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2019, June, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2019, June, 16, 12, 50).getTime(); //Same day different time
+  expect(format(dateTimeToFormat, systemDateTime)).toBe('16/06/2019');
+});
+
+it('when the system date and the date to format are not the same day formats as "15/12/2018"', () => {
+  const December = 11; //js Date object month is indexed from 0
+  const systemDateTime = new Date(2019, December, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
+  expect(format(dateTimeToFormat, systemDateTime)).toBe('15/12/2018');
+});
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,34 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  let formattedDate = new Date(dateToFormatTimeMillis);
+  let systemDate = new Date(systemDateTimeMillis);
+
+  function dateFormatted()
+  {
+      return formattedDate.getDate() + '/' + monthFormatted() + '/' + formattedDate.getFullYear();
+  }
+
+  function monthFormatted()
+  {
+    if(formattedDate.getMonth()+1 < 10)
+    {
+      return '0' + (formattedDate.getMonth()+1);
+    }
+    else
+    {
+      return formattedDate.getMonth()+1;
+    }
+  }
+
+  if(formattedDate.getFullYear() == systemDate.getFullYear())
+  {
+      if(formattedDate.getMonth() == systemDate.getMonth())
+      {
+          if(formattedDate.getDay() == systemDate.getDay())
+          {
+            return 'TODAY';
+          }
+      }
+  }
+  return dateFormatted();
+
 };
